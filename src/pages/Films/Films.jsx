ab83fa2458ab51f725693d1246230880c80a7715
mmkrivosheev <mmkrivosheev@mail.ru>
2021-11-08 19:35:08 +0300

    Add page Films

--- src/pages/Films/Films.jsx
+++ src/pages/Films/Films.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import Movies from '../../API/Movies';
+import {useFetching} from '../../hooks/useFetching';
+import FilmList from '../../components/FilmList/FilmList';
+import Loader from '../../components/UI/Loader/Loader';
+import Footer from '../../components/Footer/Footer';
+import './Films.scss';
 
 const Films = () => {
+    const totalPages = 10;
+    const [films, setFilms] = useState([]);
+    const [limit, setLimit] = useState(2);
+    const [page, setPage] = useState(1);
+
+    const [fetchFilms, isFilmsLoading, filmsError] = useFetching(async (page) => {
+        const filmsLimit = [];
+
+        for(let i = 0; i < limit; i++) {
+            const response = await Movies.getAll(page + i);
+            filmsLimit.push(...response.data.Search);
+        }
+
+        setFilms(filmsLimit);
+    });
+
+    useEffect(() => {
+        fetchFilms(page);
+    }, [page, limit]);
+
+    const changePage = (page) => {
+        setPage(page);
+    };
+
+    if(filmsError) {
+        return (
+            <div className="error">
+                <h1>Произошла ошибка ${filmsError}</h1>
+                <Footer />
+            </div>
+        );
+    }
 
     return (
-        <div>
-            Films
+        <div className="films">
+            {filmsError && <h1>Произошла ошибка ${filmsError}</h1>}
+            {isFilmsLoading
+                ? <Loader />
+                : <div>
+                    <FilmList
+                        limit={limit}
+                        setLimit={setLimit}
+                        films={films}
+                        title="Вы в разделе: Фильмы"
+                    />
+                    <Footer />
+                </div>
+            }
         </div>
     );
 };
