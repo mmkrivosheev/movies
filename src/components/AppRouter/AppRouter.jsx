28937100be64a433838faf3fcaaad6668f04da28
mmkrivosheev <mmkrivosheev@mail.ru>
2021-11-09 14:55:05 +0300

    Remove page Main

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -3,6 +3,7 @@ import {Redirect, Route, Switch} from 'react-router-dom';
 import {privateRoutes, publicRoutes} from "../../routes";
 import {AuthContext} from '../../context/context';
 import Loader from '../UI/Loader/Loader';
+import Films from "../../pages/Films/Films";
 
 const AppRouter = () => {
     const {isAuth, isLoading} = useContext(AuthContext);
@@ -23,7 +24,7 @@ const AppRouter = () => {
                         exact={route.exact}
                     />
                 )}
-                <Redirect to="/" />
+                <Redirect to="/films" />
             </Switch>
             :
             <Switch>
@@ -35,7 +36,7 @@ const AppRouter = () => {
                         exact={route.exact}
                     />
                 )}
-                <Redirect to="/" />
+                <Redirect to="/login" />
             </Switch>
     );
 };
--- src/routes.js
+++ src/routes.js
@@ -5,14 +5,12 @@ import Films from "./pages/Films/Films";
 import FilmById from "./pages/FilmById/FilmById";
 
 export const privateRoutes = [
-    {path: '/', component: Main, exact: true},
     {path: '/about', component: About, exact: true},
     {path: '/films', component: Films, exact: true},
     {path: '/films/:id', component: FilmById, exact: true}
 ];
 
 export const publicRoutes = [
-    {path: '/', component: Main, exact: true},
     {path: '/login', component: Login, exact: true},
     {path: '/about', component: About, exact: true}
 ];
