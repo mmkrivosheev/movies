c805c4c0eedbda72485c1c5530aa777c6fe36560
mmkrivosheev <mmkrivosheev@mail.ru>
2021-11-25 13:36:19 +0300

    Fix component FilmById

--- src/components/AppRouter/AppRouter.jsx
+++ src/components/AppRouter/AppRouter.jsx
@@ -3,7 +3,6 @@ import {Redirect, Route, Switch} from 'react-router-dom';
 import {privateRoutes, publicRoutes} from "../../routes";
 import {AuthContext} from '../../context/context';
 import Loader from '../UI/Loader/Loader';
-import Films from "../../pages/Films/Films";
 
 const AppRouter = () => {
     const {isAuth, isLoading} = useContext(AuthContext);
